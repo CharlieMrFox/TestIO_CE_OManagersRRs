a06de6d6dbff6bfd7dcf3026f01c142746fab686
Charlie <carlosaandresmunoz@gmail.com>
2023-08-10 11:55:06 +0800

    Create OMRR.user.js

---
+++ OMRR.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         OMRR
+// @namespace    Test IO – Education Team – Onboarding
+// @version      1.0
+// @description  Expand text abbreviations link to the official specific rejection reasons for Test IO Onboarding tickets into their full forms.
+// @icon         https://test.io/content/dam/test-io/test_io_logo1.svg
+// @match        https://onboardingmanager.test.io/managers/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Function to inject the script into the page
+    function injectScript(scriptText) {
+        const scriptElement = document.createElement('script');
+        scriptElement.textContent = scriptText;
+        document.head.appendChild(scriptElement);
+    }
+
+    // Retrieve the script from GitHub raw file
+    fetch('https://raw.githubusercontent.com/CharlieMrFox/TestIO_CE_OManagersRRs/main/TMRrs.js')
+        .then(response => response.text())
+        .then(script => {
+            injectScript(script);
+        });
+
+})();
