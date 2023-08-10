055c1886fe377c566b967fd0b77324f621bf55a6
Charlie <carlosaandresmunoz@gmail.com>
2023-08-10 12:07:55 +0800

    Create OMRR–Help.user.js
    Tampermonkey Help script link.

---
+++ OMRR–Help.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         OMRR–Help
+// @namespace    Test IO – Education Team – Onboarding
+// @author       Charlie
+// @version      1.0
+// @description  Expand text abbreviations link to the official specific rejection reasons for Test IO Onboarding tickets into their full forms.
+// @icon         https://test.io/content/dam/test-io/test_io_logo1.svg
+// @match        https://onboardingmanager.test.io/managers/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Function to inject the script into the page
+    function injectScript(scriptText) {
+        const scriptElement = document.createElement('script');
+        scriptElement.textContent = scriptText;
+        document.head.appendChild(scriptElement);
+    }
+
+    // Retrieve the script from GitHub raw file
+    fetch('https://raw.githubusercontent.com/CharlieMrFox/TestIO_CE_OManagersRRs/main/Help.js')
+        .then(response => response.text())
+        .then(script => {
+            injectScript(script);
+        });
+
+})();
